e24419a90fd5a9d8d00764c53ea0ef6d4dd531b5
Rabbithols <roberturturica@gmail.com>
2019-07-22 16:38:25 +0200

    feat: Added function to handle token
    getAccessToken: Take the token from localstorage
    
    setAccessToken: Set the token in localstorage

--- src/app/core/services/auth-service/auth.service.ts
+++ src/app/core/services/auth-service/auth.service.ts
@@ -6,4 +6,15 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  private TOKEN_KEY = 'id_token';
+
+  static getAccessToken(): string {
+    return localStorage.getItem('token');
+  }
+
+  setAccessToken(token: string): void {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
 }
