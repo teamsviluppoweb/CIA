b0be56c4990bdb89e7936a4bee85009c91e97481
Rabbithols <roberturturica@gmail.com>
2019-07-23 10:56:57 +0200

    feat: Implemented ispection jwt url and jwt extrapolation from url
    isptectUrl: Check if there is a valid jwt pattern in the url
    
    refactorUrl: Extract the jwt from the url

--- src/app/core/guards/auth-guard/auth.guard.ts
+++ src/app/core/guards/auth-guard/auth.guard.ts
@@ -1,14 +1,34 @@
 import { Injectable } from '@angular/core';
-import {CanActivate} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+    url: string;
 
-  canActivate(): Observable<any> | boolean {
-      return true;
+    constructor() {
+        this.url = window.location.href;
+    }
+
+    canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+        return true;
+    }
+
+
+    ispectUrl(url: string): boolean {
+        const myRegexp = new RegExp('[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.?[A-Za-z0-9-_.+/=]*$');
+
+        return myRegexp.test(url);
+    }
+
+    refactorUrl(url: string): string {
+        let token: string;
+
+        token = url.split('?ticket=JWT-').pop();
+
+        return token;
     }
 
 }
