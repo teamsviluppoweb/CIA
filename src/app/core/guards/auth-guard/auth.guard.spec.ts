394ee7cb63f0a0c2b9fc0398ee092858ac4bb27f
Rabbithols <roberturturica@gmail.com>
2019-07-23 10:17:20 +0200

    test: Added test for valid token in url

--- src/app/core/guards/auth-guard/auth.guard.spec.ts
+++ src/app/core/guards/auth-guard/auth.guard.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
 
@@ -9,15 +9,25 @@ describe('AuthGuard', () => {
     });
   });
 
-
-
   it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
     expect(guard).toBeTruthy();
   }));
 
 
-  it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
-    expect(guard).toBeTruthy();
+
+  it('should be true on valid token regex from cas', inject([AuthGuard], (guard: AuthGuard) => {
+    const url = 'http://172.16.26.72:4200/?ticket=JWT-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+
+    const result = AuthGuard.ispectUrl(url);
+    expect(result).toEqual(true);
+  }));
+
+
+  it('should be false on invalid token regex from cas', inject([AuthGuard], (guard: AuthGuard) => {
+    const url = 'http://172.16.26.72:4200/?ticket=JWTfdsfdsfsdxx-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+
+    const result = AuthGuard.ispectUrl(url);
+    expect(result).toEqual(false);
   }));
 
 
