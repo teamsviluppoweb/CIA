568e22a2697384fbd6c4279b8a7a6c30d4c072c8
Rabbithols <roberturturica@gmail.com>
2019-07-24 17:17:29 +0200

    chore: updated index.ts

---
+++ src/app/core/interceptors/index.ts
@@ -0,0 +1,4 @@
+export * from './token.interceptor';
+export * from './caching.interceptor';
+export * from './ensure-https.interceptor';
+export * from './logging.interceptor';
--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,1 +1,5 @@
 export * from './auth-service/auth.service';
+export * from './caching/request-cache.service'
+export * from './errorHandler/http-error-handler.service';
+export * from './logger/message.service';
+export * from './logger/logger.service';
