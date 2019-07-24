f820b2627103ee2fa2963fca3d33ade57181c4c5
Rabbithols <roberturturica@gmail.com>
2019-07-24 16:19:33 +0200

    test: added testing auth.guard and auth.service

--- src/app/core/guards/auth-guard/auth.guard.spec.ts
+++ src/app/core/guards/auth-guard/auth.guard.spec.ts
@@ -1,22 +1,58 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import {TestBed, inject, fakeAsync, async} from '@angular/core/testing';
+import {HttpClient, HttpResponse, HttpResponseBase} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import { AuthGuard } from './auth.guard';
+import {AuthService} from '../../services';
+import {Router} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
+import {routes} from '../../../app-routing.module';
+import {of} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+
+let httpClient: HttpClient;
+let httpTestingController: HttpTestingController;
+let router: Router;
+let authService: AuthService;
 
 describe('AuthGuard', () => {
+
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
-    });
+      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes(routes)],
+      providers: [
+          AuthGuard,
+        AuthService,
+      ]
+    }).compileComponents();
+
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    authService = TestBed.get(AuthService);
+    router = TestBed.get(Router);
+    router.initialNavigation();
+
   });
+
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+
+
+
 
   it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
     expect(guard).toBeTruthy();
   }));
 
 
-
   it('should be true on valid token regex from cas', inject([AuthGuard], (guard: AuthGuard) => {
-    const url = 'http://172.16.26.72:4200/?ticket=JWT-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    // tslint:disable-next-line:max-line-length
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+    const url = 'http://172.16.26.72:4200/?ticket=JWT-' + token;
 
     const result = guard.ispectUrl(url);
     expect(result).toEqual(true);
@@ -38,9 +74,6 @@ describe('AuthGuard', () => {
     const result = guard.refactorUrl(url);
     expect(result).toEqual(token);
   }));
-
-
-
 
 
 });
--- src/app/core/services/auth-service/auth.service.spec.ts
+++ src/app/core/services/auth-service/auth.service.spec.ts
@@ -1,85 +1,121 @@
-import { TestBed } from '@angular/core/testing';
+import {async, inject, TestBed} from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { routes } from '../../../app-routing.module';
+import { HttpClient } from '@angular/common/http';
+import {
+    HttpClientTestingModule,
+    HttpTestingController
+} from '@angular/common/http/testing';
+
+let authService: AuthService;
+let router: Router;
+let httpClient: HttpClient;
+let httpTestingController: HttpTestingController;
 
 describe('AuthService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule.withRoutes(routes),
+                HttpClientTestingModule,
+            ],
+            providers: [AuthService]
+        });
 
-  let authService: AuthService;
+        authService = TestBed.get(AuthService);
+        httpClient = TestBed.get(HttpClient);
+        httpTestingController = TestBed.get(HttpTestingController);
+        router = TestBed.get(Router);
+        router.initialNavigation();
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [AuthService]
+        /* Mock version of localStorage, to avoid using real one
+          https://medium.com/@armno/til-mocking-localstorage-and-sessionstorage-in-angular-unit-tests-a765abdc9d87
+
+          INFO: Note that .length property and key() method are not implemented.
+          I personally never have to use them but there should be some examples online on how to also mock them.
+     */
+        let store = {};
+        const mockLocalStorage = {
+            getItem: (key: string): string => {
+                return key in store ? store[key] : null;
+            },
+            setItem: (key: string, value: string) => {
+                store[key] = `${value}`;
+            },
+            removeItem: (key: string) => {
+                delete store[key];
+            },
+            clear: () => {
+                store = {};
+            }
+        };
+
+        spyOn(localStorage, 'getItem').and.callFake(mockLocalStorage.getItem);
+        spyOn(localStorage, 'setItem').and.callFake(mockLocalStorage.setItem);
+        spyOn(localStorage, 'removeItem').and.callFake(mockLocalStorage.removeItem);
+        spyOn(localStorage, 'clear').and.callFake(mockLocalStorage.clear);
     });
 
-    authService = TestBed.get(AuthService);
+    afterEach(() => {
+        httpTestingController.verify();
+    });
+
+    it('should be created', () => {
+        const service: AuthService = TestBed.get(AuthService);
+        expect(service).toBeTruthy();
+    });
+
+    it('should return stored token from localStorage', () => {
+        localStorage.setItem('token', 'token');
+        expect(authService.getAccessToken()).toEqual('token');
+    });
+
+    it('should store the token in localStorage', () => {
+        authService.setAccessToken('token');
+        expect(localStorage.getItem('token')).toEqual('token');
+    });
+
+    it('should return remove the token', () => {
+        authService.setAccessToken('token');
+        expect(localStorage.getItem('token')).toEqual('token');
+        localStorage.removeItem('token');
+        expect(authService.getAccessToken()).toEqual(null);
+    });
 
 
-      /* Mock version of localStorage, to avoid using real one
-      https://medium.com/@armno/til-mocking-localstorage-and-sessionstorage-in-angular-unit-tests-a765abdc9d87
+    it(`should emit 'true' for a valid call`, async(inject([AuthService, HttpTestingController],
+        (service: AuthService, backend: HttpTestingController) => {
+            service.validateJwt().subscribe((next) => {
+                expect(next).toBeTruthy();
+                expect(next).toEqual(true);
+            });
 
-      INFO: Note that .length property and key() method are not implemented.
-      I personally never have to use them but there should be some examples online on how to also mock them.
- */
-    let store = {};
-    const mockLocalStorage = {
-        getItem: (key: string): string => {
-          return key in store ? store[key] : null;
-        },
-        setItem: (key: string, value: string) => {
-          store[key] = `${value}`;
-        },
-        removeItem: (key: string) => {
-          delete store[key];
-        },
-        clear: () => {
-          store = {};
-        }
-      };
+            backend.expectOne('/cas').flush([], {status: 200, statusText: 'Authorized'});
 
-    spyOn(localStorage, 'getItem')
-          .and.callFake(mockLocalStorage.getItem);
-    spyOn(localStorage, 'setItem')
-          .and.callFake(mockLocalStorage.setItem);
-    spyOn(localStorage, 'removeItem')
-          .and.callFake(mockLocalStorage.removeItem);
-    spyOn(localStorage, 'clear')
-          .and.callFake(mockLocalStorage.clear);
-  });
+    })));
+
+    fit(`should emit 'false' for an unauthorized code and redirect to auth/login`, async(inject([AuthService, HttpTestingController],
+        (service: AuthService, backend: HttpTestingController) => {
 
 
+            authService = new AuthService(router, httpClient);
+            spyOn(router, 'navigate');
+            const spy = spyOn(service, 'logout').and.callThrough();
 
 
-  it('should be created', () => {
-    const service: AuthService = TestBed.get(AuthService);
-    expect(service).toBeTruthy();
-  });
+            service.validateJwt().subscribe((next) => {
+                expect(next).toBeFalsy();
+                expect(spy);
+                expect(service.logout).toHaveBeenCalled();
+                expect(router.navigate).toHaveBeenCalledWith(['auth/login']);
+                expect(next).toEqual(false);
+            });
 
+            backend.expectOne('/cas').flush([], {status: 401, statusText: 'Unauthorized'});
 
-  describe('setAccessToken', () => {
-    it('should store the token in localStorage',
-        () => {
-          authService.setAccessToken('token');
-          expect(localStorage.getItem('token')).toEqual('token');
-        });
-  });
-
-  describe('getAccessToken', () => {
-    it('should return stored token from localStorage',
-        () => {
-          localStorage.setItem('token', 'token');
-          expect(AuthService.getAccessToken()).toEqual('token');
-        });
-  });
-
-
-  describe('removeAccessToken', () => {
-      it('should return remove the token',
-            () => {
-                authService.setAccessToken('token');
-                expect(localStorage.getItem('token')).toEqual('token');
-                localStorage.removeItem('token');
-                expect(AuthService.getAccessToken()).toEqual(null);
-      });
-  });
+        })));
 
 });
