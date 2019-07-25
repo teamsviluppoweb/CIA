7a9af05ceb0b05323e2ae42bb07493a831001d31
Rabbithols <roberturturica@gmail.com>
2019-07-25 15:15:14 +0200

    feat: created user and guest layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SharedModule} from './shared/modules/shared.module';
+import {CoreModule} from './core';
+import { UserComponent } from './layouts/user/user.component';
+import { GuestComponent } from './layouts/guest/guest.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    GuestComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    CoreModule,
     AppRoutingModule
   ],
   providers: [],
