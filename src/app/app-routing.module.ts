e00d097d6d517c4316b640085cc02ad054244ea8
Rabbithols <roberturturica@gmail.com>
2019-07-25 15:17:24 +0200

    feat: setted correctly the routes of the layouts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthGuard} from './core/guards';
+import {UserComponent} from './layouts/user/user.component';
+import {USER_ROUTES} from './shared/routes/user.routes';
+import {GuestComponent} from './layouts/guest/guest.component';
+import {NgModule} from '@angular/core';
 
-export const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UserComponent,
+    canActivate: [AuthGuard], // Should be replaced with actual auth guard
+    children: USER_ROUTES
+  },
+  {
+    path: 'auth',
+    component: GuestComponent,
+    loadChildren: './modules/auth/auth.module#AuthModule'
+  },
+  // Fallback when no prior routes is matched
+  { path: '**', redirectTo: '/domanda/edit', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {useHash: false})],
+  exports: [RouterModule],
+  providers: []
 })
 export class AppRoutingModule { }
