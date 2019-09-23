b45da20e944ae880bd3f661719a75c099d98efe8
Rabbithols <roberturturica@gmail.com>
2019-09-23 14:05:28 +0200

    feat: Aggiunto il codice a domanda interface

---
+++ src/app/core/models/domanda.interface.ts
@@ -0,0 +1,55 @@
+export interface DomandaInterface {
+    id?: null;
+    idDomanda?: null;
+    versione: number;
+    dataInvio: string;
+    anagCandidato: AnagCandidatoInterface;
+    titoliStudioPosseduti?: (TitoliStudioPossedutiInterface)[] | null;
+    corsiAggAmm?: (CorsiAggAmmInterface)[] | null;
+    protocollo: ProtocolloInterface;
+}
+export interface AnagCandidatoInterface {
+    codiceFiscale: string;
+    cognome: string;
+    nome: string;
+    dataNascita: string;
+    codComuneNascita: string;
+    comuneNascita: string;
+    codprovNascita: string;
+    domicilio: string;
+    telefono: string;
+    email: string;
+    codQualifica: string;
+    descQualifica: string;
+    nomeQualifica: string;
+    codSede: string;
+    descSede: string;
+}
+export interface TitoliStudioPossedutiInterface {
+    idTipologia: number;
+    tipologia: string;
+    idTS: string;
+    descTS: string;
+    idIndirizzoTS: string;
+    indirizzoTS: string;
+    dataConseguimento: string;
+    istituto: string;
+    luogo: string;
+    durataAnni: string;
+}
+export interface CorsiAggAmmInterface {
+    idCorso: string;
+    tipologiaCorso: string;
+    DescCorso: string;
+    Sorgente: string;
+    GiorniCorso: string;
+    OreCorso: string;
+    dataInizio: string;
+    dataFine: string;
+    CodiceCorso: string;
+}
+export interface ProtocolloInterface {
+    id: string;
+    numero: string;
+    data: string;
+}
