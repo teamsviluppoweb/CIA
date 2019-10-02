65dd2bc19c2b53aa92807e15ba491087a5528550
Rabbithols <roberturturica@gmail.com>
2019-10-02 17:25:22 +0200

    feat: Aggiunto template mock della pagina di errore

--- src/app/modules/guest/guest.routing.ts
+++ src/app/modules/guest/guest.routing.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
+import {ErrorPageComponent} from './pages/error-page/error-page.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'error',
+    component: ErrorPageComponent
   }
 ];
 
