abae371e66691d571e64140afda84f058b0fb064
Rabbithols <roberturturica@gmail.com>
2019-11-06 17:33:28 +0100

    refactor: Rifattorizzato anagrafica component

--- src/app/modules/domanda/components/step-anagrafica/step-anagrafica.component.ts
+++ src/app/modules/domanda/components/step-anagrafica/step-anagrafica.component.ts
@@ -1,14 +1,16 @@
 import {ChangeDetectionStrategy, Component, Input, OnInit, ViewChild} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormGroup} from '@angular/forms';
 import {ApiService} from '../../../../core/services/api/api.service';
-import {MatStepper} from "@angular/material";
+import {MatStepper} from '@angular/material';
 import * as moment from 'moment';
+import {AnagCandidatoModel} from '../../../../core/models';
 
 
 @Component({
   selector: 'app-step-anagrafica',
   templateUrl: './step-anagrafica.component.html',
   styleUrls: ['./step-anagrafica.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StepAnagraficaComponent implements OnInit {
 
@@ -16,77 +18,55 @@ export class StepAnagraficaComponent implements OnInit {
   @ViewChild('stepper', { static: false }) private myStepper: MatStepper;
 
 
-  constructor(private fb: FormBuilder, private restApi: ApiService) {
-  }
+  constructor(private restApi: ApiService) {}
 
 
   ngOnInit() {
     this.onChangesForm();
 
-    this.restApi.getDomanda(false, false).subscribe(
-        (data) => {
-
-          this.anagrafica.patchValue({
-            nome: this.restApi.domanda.anagCandidato.nome,
-            cognome: this.restApi.domanda.anagCandidato.cognome,
-            dataNascita: [moment(this.restApi.domanda.anagCandidato.dataNascita).format('DD-MM-YYYY')],
-            comuneNascita: this.restApi.domanda.anagCandidato.comuneNascita.nome + ' ' + this.restApi.domanda.anagCandidato.comuneNascita.codiceProvincia,
-            domicilio: this.restApi.domanda.anagCandidato.domicilio,
-            codiceFiscale: this.restApi.domanda.anagCandidato.codiceFiscale,
-            telefono: this.restApi.domanda.anagCandidato.telefono,
-            email: this.restApi.domanda.anagCandidato.email,
-          });
-
-
-
-        }
-
-
-    );
+    const anagrafica: AnagCandidatoModel = this.restApi.domanda.anagCandidato;
+    this.anagrafica.patchValue({
+      nome: anagrafica.nome,
+      cognome: anagrafica.cognome,
+      dataNascita: [moment(anagrafica.dataNascita).format('DD-MM-YYYY')],
+      comuneNascita: anagrafica.comuneNascita.nome + ' ' + anagrafica.comuneNascita.codiceProvincia,
+      domicilio: anagrafica.domicilio,
+      codiceFiscale: anagrafica.codiceFiscale,
+      telefono: anagrafica.telefono,
+      email: anagrafica.email,
+    });
 
   }
 
+  /*  Osservo i cambiamenti del form e li applico alla classe domanda */
 
   onChangesForm() {
-    this.telefono.valueChanges.subscribe(
-        (x) => {
-          this.restApi.domanda.anagCandidato.telefono = x;
+    this.telefono.valueChanges
+        .subscribe((input) => {
+          this.restApi.domanda.anagCandidato.telefono = input;
         }
     );
 
-    this.email.valueChanges.subscribe(
-        (x) => {
-          this.restApi.domanda.anagCandidato.email = x;
+    this.email.valueChanges
+        .subscribe((input) => {
+          this.restApi.domanda.anagCandidato.email = input;
+        }
+    );
+
+    this.domicilio.valueChanges
+        .subscribe((input) => {
+          this.restApi.domanda.anagCandidato.domicilio = input;
         }
     );
   }
+
 
   get anagrafica() {
     return this.parent.get('anagrafica');
   }
 
-  get nome() {
-    return this.parent.get('anagrafica.nome');
-  }
-
-  get cognome() {
-    return this.parent.get('anagrafica.cognome');
-  }
-
-  get dataNascita() {
-    return this.parent.get('anagrafica.dataNascita');
-  }
-
-  get comuneNascita() {
-    return this.parent.get('anagrafica.comuneNascita');
-  }
-
   get domicilio() {
     return this.parent.get('anagrafica.domicilio');
-  }
-
-  get codiceFiscale() {
-    return this.parent.get('anagrafica.codiceFiscale');
   }
 
   get telefono() {
@@ -95,10 +75,6 @@ export class StepAnagraficaComponent implements OnInit {
 
   get email() {
     return this.parent.get('anagrafica.email');
-  }
-
-  get sedeServizio() {
-    return this.parent.get('anagrafica.sedeServizio');
   }
 
 
